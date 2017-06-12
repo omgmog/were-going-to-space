cfab0b7394511c19ed8c60a9af473f582d97e233
Max Glenister <moglenstar@gmail.com>
2017-06-12 21:48:42 +0100

    Rewrite

---
+++ js/game.js
@@ -0,0 +1,190 @@
+var game = (function () {
+  'use strict';
+
+  /////////////////////////////////////////////////////////////////////////////
+  //// UTILS
+  /////////////////////////////////////////////////////////////////////////////
+  var utils = {};
+
+  utils.pi = Math.PI;
+  utils.tau =  Math.PI / 2;
+  utils.pi2 = Math.PI * 2;
+
+
+  // graphics update
+  utils.render = function (delta) {
+    core.controls.update(delta);
+    TWEEN.update();
+    // Per frame stuff here
+
+    core.renderer.render(core.scene, core.camera);
+
+    // if we've got an effect, render it
+    if (core.effect !== null) {
+      core.effect.render(core.scene, core.camera);
+    }
+  };
+
+  // logic update
+  utils.update = function (delta) {
+
+  }
+
+  utils.animate = function () {
+    requestAnimationFrame(utils.animate);
+    utils.update(core.clock.getDelta());
+    utils.render(core.clock.getDelta());
+  };
+
+  utils.append = function (things, parent) {
+    if (!(things instanceof Array)) {
+      things = [things];
+    }
+    things.forEach(function (thing) {
+      parent.add(thing);
+    });
+  };
+  utils.haveHMD = function () {
+    if ("getVRDisplays" in navigator) {
+      navigator.getVRDisplays()
+        .then(function (displays) {
+          if (displays.length > 0) {
+            return true;
+          } else {
+            // No HMDs detected
+            return false;
+          }
+        })
+        .catch(function (err) {
+          // Something went wrong, so bail out
+          return false;
+        });
+
+    } else {
+      // WebVR not supported
+      return false;
+    }
+  };
+
+  utils.onMobileDevice = function () {
+    try{ document.createEvent("TouchEvent"); return true; }
+    catch(e){ return false; }
+  };
+
+  utils.onWindowResize = function () {
+    var wiw = window.innerWidth;
+    var wih = window.innerHeight;
+    core.camera.aspect = wiw / wih;
+    core.camera.updateProjectionMatrix();
+    core.renderer.setSize(wiw, wih);
+
+    if (core.effect !== null) {
+      core.effect.setSize(wiw, wih);
+    }
+  };
+
+  utils.construct = function (constructor, args) {
+    function F() {return constructor.apply(this, args)};
+    F.prototype = constructor.prototype;
+    return new F();
+  };
+
+  utils.build = function (geotype, geoprops, mattype, matprops) {
+    return new T.Mesh(
+      utils.construct(T[geotype], geoprops),
+      utils.construct(T[mattype], matprops)
+    );
+  };
+
+  // Convert deg to rad
+  utils.d2r = function (degrees) {
+    return degrees * utils.pi / 180;
+  };
+
+  utils.scaleVec2 = function (vec2, scale) {
+    return new T.Vector2(vec2.x * scale, vec2.y * scale);
+  };
+
+  utils.scaleVec3 = function (vec3, scale) {
+    return new T.Vector3(vec3.x * scale, vec3.y * scale, vec3.z * scale);
+  };
+
+  utils.flipVertical = function () {
+    return new T.Matrix4().makeRotationX(utils.pi);
+  };
+
+  utils.debugAxis = function () {
+    utils.append(new T.AxisHelper(100), core.scene);
+  };
+
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  //// CORE
+  /////////////////////////////////////////////////////////////////////////////
+  var core = {};
+
+  var T = THREE; // Shorthand to THREE built-ins
+
+  var init = function () {
+    core.clock = new T.Clock();
+    core.cameraHeight = 50;
+    core.distance = 1000;
+    core.fov = 50; // fov in Three is vertical, so 40-50 vfov is about 70 in hfov
+    core.camera = new T.PerspectiveCamera(core.fov, window.innerWidth / window.innerHeight, 0.1, core.distance);
+    core.effect = null;
+    core.scene = new T.Scene();
+    core.scene.fog = new T.FogExp2(0x000000, 0.002);
+    core.renderer = new T.WebGLRenderer({
+      antialias: true,
+      gammaInput: true,
+      gammaOutput: true
+    });
+    core.renderer.setClearColor(core.scene.fog.color);
+    core.renderer.setPixelRatio(window.devicePixelRatio);
+    core.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Shadow setup
+    core.renderer.shadowMap.enabled = true;
+    core.renderer.shadowMapSoft = true;
+    core.renderer.shadowCameraNear = 3;
+    core.renderer.shadowCameraFar = core.camera.far;
+    core.renderer.shadowCameraFov = 50;
+    core.renderer.shadowMapBias = 0.0039;
+    core.renderer.shadowMapDarkness = 0.5;
+    core.renderer.shadowMapWidth = 1024;
+    core.renderer.shadowMapHeight = 1024;
+
+    if (utils.haveHMD()) {
+      core.controls = new T.VRControls(core.camera);
+      console.log('Using HMD');
+    } else if (utils.onMobileDevice()) {
+      core.effect = new T.StereoEffect(core.renderer);
+      core.controls = new T.DeviceOrientationControls(core.camera, true);
+      console.log('Using DeviceOrientationControls');
+    } else {
+      // OrbitControls for the peasants.
+      core.controls = new T.OrbitControls(core.camera, core.renderer.domElement);
+      console.log('Using mouse');
+    }
+    core.camera.position.y = core.cameraHeight;
+    if (typeof core.controls.userHeight !== "undefined") {
+      console.log("Setting camera height using userHeight");
+      core.camera.position.y = core.controls.userHeight;
+    }
+
+    core.camera.updateProjectionMatrix();
+
+    // Initialise it
+    window.addEventListener('resize', utils.onWindowResize, false);
+    document.body.appendChild(core.renderer.domElement);
+    utils.animate();
+  };
+
+  init();
+
+  return {
+    core: core,
+    utils: utils,
+  }
+})();
---
+++ js/stage.js
@@ -0,0 +1,435 @@
+(function () {
+  'use strict';
+
+  var T = THREE;
+  var utils = game.utils;
+  var core = game.core;
+
+  utils.debugAxis();
+
+  // floor
+
+  var floor = utils.build(
+    'PlaneGeometry', [1000, 1000, 10, 10],
+    'MeshPhongMaterial', [{
+      color: 0x1a1a1a,
+      shading: T.SmoothShading,
+      side: T.DoubleSide,
+    }]
+  );
+  floor.position.y = 0;
+  floor.rotation.x = utils.tau;
+
+  utils.append(floor, core.scene);
+
+
+  // rocket
+  var rocket = new T.Object3D();
+  rocket.position.set(-130, 125, -97);
+  // rocket.position.set(0, 125, 0);
+  // rocket.rotation.set(0, 19.9, 0); // do this with matrix transform?
+  rocket.rotation.y = utils.d2r(45);
+  var rocketGeo = [
+    new T.Vector2(0, 0),
+    new T.Vector2(0.5, 0.2),
+    new T.Vector2(1.29992285, 1.43232647),
+    new T.Vector2(2.14964952, 3.30962958),
+    new T.Vector2(2.76224316, 5.44382679),
+    new T.Vector2(2.9796151, 6.86662493),
+    new T.Vector2(2.99937619, 8.23013982),
+    new T.Vector2(2.99937619, 9.35652168),
+    new T.Vector2(2.72027206, 11.6990187),
+    new T.Vector2(2.27251293, 13.3075548),
+    new T.Vector2(1.61962198, 14.6639325),
+    new T.Vector2(1.45801153, 17.3400002),
+    new T.Vector2(0, 17.3400002)
+  ];
+  var rocketBody = utils.build(
+    'LatheGeometry', [rocketGeo, 128],
+    'MeshPhongMaterial', [{
+      color: 0xeeeeee,
+      shading: T.SmoothShading,
+      specular: 0xffffff,
+      shininess: 90,
+    }]
+  );
+  rocketBody.geometry.applyMatrix(utils.flipVertical());
+  rocketBody.geometry.scale(7,7,7);
+
+  utils.append(rocketBody, rocket);
+
+  var wingGeo = new T.Shape();
+  wingGeo.moveTo(0, 0);
+  wingGeo.lineTo(0.881591797, 0.325592041);
+  wingGeo.lineTo(1.67909632, 0.998413443);
+  wingGeo.lineTo(2.30402536, 1.8873078);
+  wingGeo.lineTo(2.65497254, 2.78481932);
+  wingGeo.lineTo(2.78297804, 3.8601981);
+  wingGeo.lineTo(2.78297804, 5);
+  wingGeo.lineTo(2.04452515, 3.91278076);
+  wingGeo.lineTo(1.4347229, 3.29016113);
+  wingGeo.lineTo(0.75402832, 2.84954834);
+  wingGeo.lineTo(0.00935368156, 2.68509582);
+
+  var wing = utils.build(
+    'ExtrudeGeometry', [wingGeo, {
+      amount: .2,
+      bevelEnabled: false,
+    }],
+    'MeshPhongMaterial', [{
+      color: 0xdd0000,
+      shading: T.SmoothShading,
+    }]
+  );
+  wing.geometry.applyMatrix(utils.flipVertical());
+  wing.scale.set(10,10,10);
+
+
+  var wings = Array(3);
+  var wingsTotal = wings.length;
+  var wingStep = utils.d2r(360 / wingsTotal);
+
+  for (var i=0; i<wingsTotal; i++) {
+    wings[i] = wing.clone();
+    wings[i].rotation.y = wingStep * i;
+    wings[i].position.y = -75;
+  }
+
+  utils.append(wings, rocket);
+
+  // door?
+
+  utils.append(rocket, core.scene);
+
+
+  // conveyor
+  var conveyor = new T.Object3D();
+  var conveyorProps = {
+    length: 170,
+    width: 10,
+    wheelSize: 3
+  };
+  // Could this be done with a matrix transform?
+  conveyor.rotation.set(0, -.2, -.1);
+  conveyor.position.set(-30, 23, -77);
+
+  var belt = utils.build(
+    'BoxGeometry', [conveyorProps.length, 1, conveyorProps.width],
+    'MeshPhongMaterial', [{
+      color: 0x777777,
+      shading: T.SmoothShading,
+    }]
+  );
+  var belts = [];
+  belts.push(belt.clone());
+  belts.push(belt.clone());
+
+  belts[0].position.y = conveyorProps.wheelSize;
+  belts[1].position.y = -conveyorProps.wheelSize;
+
+  var wheel = utils.build(
+    'CylinderGeometry', [conveyorProps.wheelSize, conveyorProps.wheelSize, conveyorProps.width - 2, 16],
+    'MeshPhongMaterial', [{
+      color: 0xeeeeee,
+      shading: T.SmoothShading,
+    }]
+  );
+  wheel.rotation.x = utils.tau;
+
+  var maxWheels = Math.floor(conveyorProps.length / (conveyorProps.wheelSize * 4));
+  var wheels = [];
+
+  for (var i=0; i<=maxWheels; i++) {
+    var _wheel = wheel.clone();
+    _wheel.position.x = ((conveyorProps.length/2) - conveyorProps.length) + (i * (conveyorProps.length / maxWheels));
+    wheels.push(_wheel);
+  }
+
+  utils.append(belts, conveyor);
+  utils.append(wheels, conveyor);
+
+  utils.append(conveyor, core.scene);
+
+
+  // table
+
+  var table = new T.Object3D();
+
+  var tableProps = {
+    width: 45,
+    height: 20,
+    depth: 15,
+    topThickness: 1,
+    legThickness: 1,
+    legsCount: 4,
+    color: 0xaaaaaa,
+  };
+  table.position.y = tableProps.height;
+  table.position.z = -40;
+  table.position.x = -20;
+
+  var tableTop = utils.build(
+    'BoxGeometry', [tableProps.width, tableProps.topThickness, tableProps.depth],
+    'MeshPhongMaterial', [{
+      color: tableProps.color,
+      shading: T.SmoothShading,
+    }]
+  );
+  utils.append(tableTop, table);
+  var tableLeg = utils.build(
+    'CylinderGeometry', [
+      tableProps.legThickness,
+      tableProps.legThickness,
+      tableProps.height - tableProps.topThickness,
+      8
+    ],
+    'MeshPhongMaterial', [{
+      color: tableProps.color,
+      shading: T.SmoothShading,
+    }]
+  );
+  var tableLegs = [];
+  var tableLegPositions = [
+    [-(tableProps.width / 2) + tableProps.legThickness, -(tableProps.depth / 2) + tableProps.legThickness],
+    [-(tableProps.width / 2) + tableProps.legThickness, (tableProps.depth / 2) - tableProps.legThickness],
+    [(tableProps.width / 2) - tableProps.legThickness, -(tableProps.depth / 2) + tableProps.legThickness],
+    [(tableProps.width / 2) - tableProps.legThickness, (tableProps.depth / 2) - tableProps.legThickness]
+  ];
+
+  for (var i=0; i<tableProps.legsCount; i++) {
+    var _tableLeg = tableLeg.clone();
+    _tableLeg.position.set(
+      tableLegPositions[i][0],
+      -1 * (tableProps.height / 2),
+      tableLegPositions[i][1]
+    );
+    tableLegs.push(_tableLeg);
+  }
+  utils.append(tableLegs, table);
+
+    // computer
+
+    var computer = new T.Object3D();
+
+    computer.position.y = 1;
+    computer.position.x = 8;
+
+    var monitor = new T.Object3D();
+    var monitorShell = utils.build(
+      'BoxGeometry', [16, 12, 5],
+      'MeshPhongMaterial', [{
+        color: 0x333333,
+        shading: T.SmoothShading,
+      }]
+    );
+    var monitorStand = utils.build(
+      'CylinderGeometry', [2, 2, 2, 8],
+      'MeshPhongMaterial', [{
+        color: 0x333333,
+        shading: T.SmoothShading,
+      }]
+    );
+    monitorStand.position.y = -7;
+
+    var monitorScreen = utils.build(
+      'PlaneGeometry', [14, 10, 1],
+      'MeshPhongMaterial', [{
+        color: 0x000000,
+        shading: T.SmoothShading,
+      }]
+    );
+    monitorScreen.position.z = 2.6;
+
+
+    monitor.position.x = 1;
+    monitor.position.y = 7;
+    monitor.position.z = -3;
+    monitor.rotation.y = utils.d2r(-8);
+
+    utils.append(monitorShell, monitor);
+    utils.append(monitorScreen, monitor);
+    utils.append(monitorStand, monitor);
+
+    utils.append(monitor, computer);
+
+
+    var keyboard = new T.Object3D();
+    var keyProps = {
+      total: 70,
+      wrap: 14,
+      width: .6,
+      height: .5,
+      gutter: .4,
+    };
+
+    var keyboardProps = {
+      depth: 1 + ((keyProps.total / keyProps.wrap) * (keyProps.width + keyProps.gutter)),
+      width: 1 + (keyProps.wrap * (keyProps.width + keyProps.gutter)),
+      color: 0x333333,
+    };
+    keyboard.position.z = 3;
+    keyboard.rotation.y = utils.d2r(-10);
+
+    var keyboardBase = utils.build(
+      'BoxGeometry', [keyboardProps.width, 1, keyboardProps.depth],
+      'MeshPhongMaterial', [{
+        color: keyboardProps.color,
+        shading: T.SmoothShading,
+      }]
+    );
+    utils.append(keyboardBase, keyboard);
+
+    var key = utils.build(
+      'CylinderGeometry', [keyProps.width * .66, keyProps.width, keyProps.height, 4],
+      'MeshPhongMaterial', [{
+        color: keyboardProps.color,
+        shading: T.SmoothShading,
+      }]
+    );
+    key.rotation.y = utils.d2r(45);
+    key.updateMatrix();
+
+    var keys = [];
+    var rowCount = 0;
+    var colCount = 0;
+
+    for (var i=0; i<keyProps.total; i++) {
+      var _key = key.clone();
+      if (i % keyProps.wrap === 0) {
+        rowCount++;
+        colCount = 0;
+      } else {
+        colCount++;
+      }
+
+      var singleKey = keyProps.width + keyProps.gutter;
+
+      _key.position.set(
+        (colCount * singleKey) - ((keyboardProps.width / 2) - singleKey),
+        .75,
+        rowCount + singleKey - (keyboardProps.depth * .65)
+      );
+
+      // Make ESC red
+      if (i === 0) {
+        _key.material = key.material.clone();
+        _key.material.color.setHex(0xdd0000);
+      }
+      // Make a space bar
+      if (i === 60) {
+        _key.geometry = new T.BoxGeometry((6 * singleKey) - .2,.5,.7);
+        _key.position.x = _key.position.x + (singleKey * 2.5);
+        _key.position.z = _key.position.z + .05;
+        _key.rotation.y = utils.d2r(0)
+        _key.rotation.x = utils.d2r(-10);
+      }
+      // destroy these
+      if (i > 60 && i < 66) {
+        _key = new T.Object3D();
+      }
+      keys.push(_key);
+    }
+    utils.append(keys, keyboard);
+
+
+    utils.append(keyboard, computer);
+
+    utils.append(computer, table);
+
+
+    // paper
+
+    utils.append(table, core.scene);
+
+  // cupboards
+
+  var cupboard = new T.Object3D();
+  var cupboardProps = {
+    shelves: 3,
+    width: 45,
+    depth: 10,
+    height: 30,
+    legs: 4,
+    legWidth: 1,
+  };
+
+  var shelf = utils.build(
+    'BoxGeometry', [cupboardProps.width, 1, cupboardProps.depth],
+    'MeshPhongMaterial', [{
+      color: 0xff0000,
+      shading: T.SmoothShading,
+    }]
+  );
+  var shelves = [];
+  for (var i=0; i <cupboardProps.shelves; i++) {
+    var _shelf = shelf.clone();
+    _shelf.position.y = 10 + (i * 10);
+
+    shelves.push(_shelf);
+  }
+  utils.append(shelves, cupboard);
+
+
+  var cupboardLeg = utils.build(
+    'CylinderGeometry', [cupboardProps.legWidth, cupboardProps.legWidth, cupboardProps.height, 8],
+    'MeshPhongMaterial', [{
+      color: 0xdd0000,
+      shading: T.SmoothShading,
+    }]
+  );
+  var cupboardLegPositions = [
+    [-(cupboardProps.width / 2) + cupboardProps.legWidth, -(cupboardProps.depth / 2) + cupboardProps.legWidth],
+    [-(cupboardProps.width / 2) + cupboardProps.legWidth, (cupboardProps.depth / 2) - cupboardProps.legWidth],
+    [(cupboardProps.width / 2) - cupboardProps.legWidth, -(cupboardProps.depth / 2) + cupboardProps.legWidth],
+    [(cupboardProps.width / 2) - cupboardProps.legWidth, (cupboardProps.depth / 2) - cupboardProps.legWidth]
+  ];
+  var cupboardLegs = [];
+  for (var i=0; i <cupboardProps.legs; i++) {
+    var _cupboardLeg = cupboardLeg.clone();
+    _cupboardLeg.position.y = (cupboardProps.height / 2);
+    _cupboardLeg.position.x = cupboardLegPositions[i][0];
+    _cupboardLeg.position.z = cupboardLegPositions[i][1];
+
+    shelves.push(_cupboardLeg);
+  }
+  utils.append(cupboardLegs, cupboard);
+  utils.append(shelves, cupboard);
+
+
+  var totalCupboards = 3;
+  var cupboards = [];
+
+  var cupboardRadius = 100;
+  var cupboardStep = utils.d2r(30);
+  var cupboardAngle = utils.d2r(120);
+
+  for (var i=0; i<totalCupboards; i++) {
+    var _cupboard = cupboard.clone();
+    _cupboard.position.y = 1;
+    _cupboard.position.x = Math.cos(cupboardAngle) * cupboardRadius;
+    _cupboard.position.z = Math.sin(cupboardAngle) * cupboardRadius;
+
+    cupboardAngle += cupboardStep;
+
+    _cupboard.lookAt(new T.Vector3(0,0,0));
+    cupboards.push(_cupboard);
+  }
+  utils.append(cupboards, core.scene);
+
+
+
+
+
+  // lights
+  var ambient = new T.AmbientLight(0xbbbbbb);
+  utils.append(ambient, core.scene);
+
+  var directional = new T.DirectionalLight(0xffffff, .4);
+  directional.castShadow = true;
+  directional.position.z = -10;
+  directional.position.y = 50;
+  directional.lookAt(rocket);
+
+  utils.append(directional, core.scene);
+
+}());
