137238e3bf2c7b03517cb8ece0842ac4cb0fa274
Max <moglenstar@gmail.com>
2017-06-20 11:50:17 +0100

    removed debug crosshair

--- js/game.js
+++ js/game.js
@@ -132,7 +132,6 @@ var game = (function () {
 
   utils.debugAxis = function () {
     utils.append(new T.AxisHelper(100), core.scene);
-    utils.createCrosshair(core.camera);
   };
   utils.do = function (count, fn) {
     for (var i=0; i<count; i++) {
@@ -253,37 +252,6 @@ var game = (function () {
         });
       }
     });
-  };
-
-  utils.createCrosshair = function (camera) {
-    var material = new THREE.LineBasicMaterial({ color: 0xAAFFAA });
-
-    // crosshair size
-    var x = 0.01, y = 0.01;
-
-    var geometry = new THREE.Geometry();
-
-    // crosshair
-    geometry.vertices.push(new THREE.Vector3(0, y, 0));
-    geometry.vertices.push(new THREE.Vector3(0, -y, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(x, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(-x, 0, 0));
-
-    var crosshair = new THREE.Line( geometry, material );
-
-    // place it in the center
-    var crosshairPercentX = 50;
-    var crosshairPercentY = 50;
-    var crosshairPositionX = (crosshairPercentX / 100) * 2 - 1;
-    var crosshairPositionY = (crosshairPercentY / 100) * 2 - 1;
-
-    crosshair.position.x = crosshairPositionX * camera.aspect;
-    crosshair.position.y = crosshairPositionY;
-
-    crosshair.position.z = -0.3;
-
-    camera.add( crosshair );
   };
 
   utils.setupRaycaster = function () {
